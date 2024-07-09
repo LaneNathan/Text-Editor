e1749644cdd8a3ceca9e4f22975fbd801505e33a
Nathan Lane <lanenathan214@gmail.com>
2024-07-08 22:47:37 -0400

    Add CSS loaders and babel to webpack configuration

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
+
 
 module.exports = () => {
   return {
@@ -45,7 +45,20 @@ module.exports = () => {
     ],
     module: {// CSS loaders and babel to webpack.
       rules: [
-        
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+      },
+    },
+  },
       ],
     },
   };

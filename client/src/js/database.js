b8a0c6dc9a054947a76dbcd1c9c885a4db8b0aba
Nathan Lane <lanenathan214@gmail.com>
2024-07-10 20:24:37 -0400

    updated putDb function to include an ID for content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put(content);
+  const request = store.put({id:1, content});
   const result = await request;
   console.log('putDb result:', result);
 };

5786cc6cfea0de7b49c609919a80b68faf2ee331
Nathan Lane <lanenathan214@gmail.com>
2024-07-10 20:21:33 -0400

    updated service worker stragegies

--- client/src-sw.js
+++ client/src-sw.js
@@ -2,7 +2,7 @@ const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+import { warmStrategyCache } from 'workbox-recipes';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
